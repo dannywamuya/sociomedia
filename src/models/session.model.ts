4984a1d812b820e8ac0f6984c1565bad255fbba7
unknown <dwamuya@gmail.com>
2023-01-06 14:30:42 +0300

    feat: added session model

---
+++ src/models/session.model.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { IUser } from "./user.model";
+
+export interface ISession {
+  user: IUser;
+  valid: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const sessionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "User",
+    },
+    valid: { type: Boolean },
+  },
+  { timestamps: true }
+);
+
+const SessionModel = mongoose.model<ISession>("Session", sessionSchema);
+
+export default SessionModel;
