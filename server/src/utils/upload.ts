278c6a69e14ab4769607bbb6f2f1196873408b92
Danny Wamuya <dwamuya@gmail.com>
2023-01-12 09:46:58 +0300

    feat: added error catching middleware for max file size for images

--- server/src/utils/upload.ts
+++ server/src/utils/upload.ts
@@ -1,11 +1,24 @@
 import util from "util";
-import Multer from "multer";
+import Multer, { MulterError } from "multer";
+import { NextFunction, Request, Response } from "express";
 
 const SINGLE_FILE_LIMIT = 2 * 1024 * 1024;
 
-export const uploadSingleFileMiddleWare = util.promisify(
-  Multer({
-    storage: Multer.memoryStorage(),
-    limits: { fileSize: SINGLE_FILE_LIMIT },
-  }).single("file")
-);
+export const uploadSingleFileMiddleWare = [
+  util.promisify(
+    Multer({
+      storage: Multer.memoryStorage(),
+      limits: { fileSize: SINGLE_FILE_LIMIT },
+    }).single("file")
+  ),
+  (err: MulterError, req: Request, res: Response, next: NextFunction) => {
+    console.log(err.message, "err");
+    if (err.code === "LIMIT_FILE_SIZE") {
+      return res.status(400).send({
+        message: `Error: ${err.message} The file should be less than 2MB.`,
+      });
+    } else {
+      return next(err);
+    }
+  },
+];
