ee633ea8d74094db7aed7c279086883fe02fe68b
Danny Wamuya <dwamuya@gmail.com>
2023-01-14 22:14:07 +0300

    feat: added cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,8 +5,15 @@ import connectDB from "./utils/connectDb";
 import logger from "./utils/logger";
 import router from "./routes";
 import deserializeUser from "./middleware/deserializeUser";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 app.use(deserializeUser);
 app.use("/api", router);
@@ -14,6 +21,6 @@ app.use("/api", router);
 const port = config.get<number>("port");
 
 app.listen(port, async () => {
-  logger.info(`App is listening on http://localhost:${port}`);
+  logger.info(`App is listening...`);
   await connectDB();
 });
