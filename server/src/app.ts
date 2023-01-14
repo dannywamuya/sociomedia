49600fb2f482cffe575d9d321071266b82133cc5
Danny Wamuya <dwamuya@gmail.com>
2023-01-14 22:34:28 +0300

    feat: added origin url to env

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:5173"
+        : process.env.ORIGIN_URL,
   })
 );
 app.use(express.json());
